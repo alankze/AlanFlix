823518d4c5e84653bbf29b22f5dbde693cd9e378
alankze <alan.zin.eddin@gmail.com>
2023-08-03 03:31:38 +0200

    FlixApp

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "3c7f6c8833ca72c9f55380e9bfad9e1a";
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
